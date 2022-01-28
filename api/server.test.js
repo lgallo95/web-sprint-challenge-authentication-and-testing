b4334d4c4e28337c81eae32c56d3a451482ab214
lgallo95 <lgallo2005@gmail.com>
2022-01-27 23:28:52 -0800

    done

--- api/server.test.js
+++ api/server.test.js
@@ -8,45 +8,36 @@ beforeAll(async () => {
   await db.migrate.latest();
 });
 beforeEach(async () => {
-  await db.seed.run()
-})
+  await db.seed.run();
+});
 
 afterAll(async () => {
   await db.destroy();
 });
 
-
 describe("Auth Tests", () => {
-  describe("New User Register Successful!!!", () => {
-    test('Successful Registration returns 200', async () =>{
-      const res = await request(server).post('/api/auth/register')
-      .send({username: "Fillsad", password: "ashdfgasdojuhif"})
-      expect(res.status).toBe(200)
-    })
+  test("Successful Registration returns 200", async () => {
+    const res = await request(server)
+      .post("/api/auth/register")
+      .send({ username: "Fillsad", password: "ashdfgasdojuhif" });
+    expect(res.status).toBe(200);
   });
-  test('welcomes New User', async () => {
+  test("welcomes New User", async () => {
     const res = await request(server)
-    .post('/api/auth/register')
-    .send({username: "Fillsad", password: "ashdfgasdojuhif"})
-    expect(res.body.message).toBe("Welcome, Fillsad")
-  })
-  test("Successful Login", async() => {
+      .post("/api/auth/register")
+      .send({ username: "Fillsad", password: "ashdfgasdojuhif" });
+    expect(res.body.message).toBe("Welcome, Fillsad");
+  });
+  test("Successful Login", async () => {
     const res = await request(server)
-    .post('/api/auth/login')
-    .send({username: "sam", password: "isadfhoasdjfnj"})
-    expect(res.status).toBe(200)
-  })
-  test("401 if wrong info", async() => {
+      .post("/api/auth/login")
+      .send({ username: "sam", password: "isadfhoasdjfnj" });
+    expect(res.status).toBe(200);
+  });
+  test("401 if wrong info", async () => {
     const res = await request(server)
-    .post('/api/auth/login')
-    .send({username: "sam", password: "assadfasdf"})
-    expect(res.status).toBe(401)
-  })
+      .post("/api/auth/login")
+      .send({ username: "sam", password: "assadfasdf" });
+    expect(res.status).toBe(401);
+  });
 });
-
-describe("Joke Tests", async () => {
-  const res = await request(server)
-  .post('/api/auth/register')
-  .send({username: "Fillsad", password: "ashdfgasdojuhif"})
-  .get('/api/jokes')
-})
